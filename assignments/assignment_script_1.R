cb71ac1ea5c62757f7098edb0770ddcd7cb2eb5e
Tyshawn Rene <tyshawnrene@gmail.com>
2025-01-15 15:12:13 -0500

    Added Assignment 1

---
+++ assignments/assignment_script_1.R
@@ -0,0 +1,13 @@
+db = DB |> 
+  dplyr::filter(scenario == "RCP45", 
+                         year == 2055,
+                         interval == "mon")
+x = read_brickman(db)
+buoys = gom_buoys()
+M01 = dplyr::filter(buoys, id == "M01")
+long_values = extract_brickman(x, M01)
+SST = dplyr::filter(long_values, name=="SST")
+ggplot(data = SST,                               
+       mapping = aes(x = month, y = value)) +                
+  geom_point() +                                     
+  labs(title = "RCP4.5 2055 SST at Buoy M01")
