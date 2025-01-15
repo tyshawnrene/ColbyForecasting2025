20295b759b341d9098804b69d02dd956dff7b286
Tyshawn Rene <tyshawnrene@gmail.com>
2025-01-15 15:14:26 -0500

    Fixed issue in Assignment 1

--- assignments/assignment_script_1.R
+++ assignments/assignment_script_1.R
@@ -8,6 +8,6 @@ M01 = dplyr::filter(buoys, id == "M01")
 long_values = extract_brickman(x, M01)
 SST = dplyr::filter(long_values, name=="SST")
 ggplot(data = SST,                               
-       mapping = aes(x = month, y = value)) +                
+       mapping = aes(x = factor(month, month.abb), y = value)) +                
   geom_point() +                                     
   labs(title = "RCP4.5 2055 SST at Buoy M01")
